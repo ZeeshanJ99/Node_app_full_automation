1a039170bee111d9150829a22e0aa5cbe4ee0938
WMoorby <WMoorby@spartaglobal.com>
2021-09-24 14:21:39 +0100

    Added all required metric to Cloudwatch

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
                 "type":"metric",
                 "x":0,
                 "y":0,
-                "width":12,
+                "width":24,
                 "height":6,
                 "properties":{
                     "metrics":[
@@ -30,7 +30,7 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
                     "stat":"Average",
                     "region":"eu-west-1",
                     "title":"App's Average CPU",
-                    "liveData": false,
+                    "liveData": true,
                     "legend": {
                         "position": "right"
                     }
@@ -41,7 +41,7 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
                 "type":"metric",
                 "x":0,
                 "y":1,
-                "width":12,
+                "width":24,
                 "height":6,
                 "properties":{
                     "metrics":[
@@ -61,8 +61,132 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
                     "period":300,
                     "stat":"Average",
                     "region":"eu-west-1",
-                    "title":"App's Average CPU",
-                    "liveData": false,
+                    "title":"App's Average Network",
+                    "liveData": true,
+                    "legend": {
+                        "position": "right"
+                    }
+                
+                }
+            },
+            {
+                "type":"metric",
+                "x":0,
+                "y":2,
+                "width":24,
+                "height":6,
+                "properties":{
+                    "metrics":[
+                    [
+                        "AWS/AutoScalingGroup",
+                        "GroupTotalInstances",
+                        "AutoScalingGroupName",
+                        "sre-viktor-tf-asg"
+                    ],
+                    [
+                        "AWS/AutoScalingGroup",
+                        "GroupPendingInstances",
+                        "AutoScalingGroupName",
+                        "sre-viktor-tf-asg"
+                    ]
+                    ],
+                    "period":300,
+                    "stat":"Average",
+                    "region":"eu-west-1",
+                    "title":"No. of Instances",
+                    "liveData": true,
+                    "legend": {
+                        "position": "right"
+                    }
+                
+                }
+            },
+            {
+                "type":"metric",
+                "x":0,
+                "y":3,
+                "width":24,
+                "height":6,
+                "properties":{
+                    "metrics":[
+                    [
+                        "AWS/ApplicationELB",
+                        "RequestCount"
+                    ],
+                    [
+                        "AWS/ApplicationELB",
+                        "RequestCountPerTarget"
+                    ]
+                    ],
+                    "period":300,
+                    "stat":"Average",
+                    "region":"eu-west-1",
+                    "title":"Request Count",
+                    "liveData": true,
+                    "legend": {
+                        "position": "right"
+                    }
+                
+                }
+            },
+            {
+                "type":"metric",
+                "x":0,
+                "y":4,
+                "width":24,
+                "height":6,
+                "properties":{
+                    "metrics":[
+                    [
+                        "AWS/EC2",
+                        "AutoScalingGroupName",
+                        "sre-viktor-tf-asg",
+                        "DiskReadOps"
+                    ],
+                    [
+                        "AWS/EC2",
+                        "AutoScalingGroupName",
+                        "sre-viktor-tf-asg",
+                        "DiskWriteOps"
+                    ],
+                    ],
+                    "period":300,
+                    "stat":"Average",
+                    "region":"eu-west-1",
+                    "title":"Read/Write Ops",
+                    "liveData": true,
+                    "legend": {
+                        "position": "right"
+                    }
+                
+                }
+            },
+            {
+                "type":"metric",
+                "x":0,
+                "y":5,
+                "width":24,
+                "height":6,
+                "properties":{
+                    "metrics":[
+                    [
+                        "AWS/EC2",
+                        "AutoScalingGroupName",
+                        "sre-viktor-tf-asg",
+                        "DiskReadBytes"
+                    ],
+                    [
+                        "AWS/EC2",
+                        "AutoScalingGroupName",
+                        "sre-viktor-tf-asg",
+                        "DiskWriteBytes"
+                    ],
+                    ],
+                    "period":300,
+                    "stat":"Average",
+                    "region":"eu-west-1",
+                    "title":"Read/Write Bytes",
+                    "liveData": true,
                     "legend": {
                         "position": "right"
                     }
