7e3117325f02a7c73e54fc2f21e6f55a32a523d9
Sacha <sdorf@spartaglobal.com>
2021-09-24 16:12:25 +0100

    Added Terraform files to set up Grafana EC2 Instance plus documentation

---
+++ terraform/variable.tf
@@ -0,0 +1,28 @@
+variable "vpc_id" {
+  default = "vpc-07e47e9d90d2076da"
+}
+
+variable "ami_grafana_id" {
+  default = "ami-0943382e114f188e8"
+}
+ 
+variable "aws_key_name" {
+    default = "sre_SDMTVM_key"
+
+}
+
+variable "aws_key_path" {
+    default = "~/.ssh/sre_SDMTVM_key.pem"
+}
+
+variable "subnet_public_id" {
+
+    default = "subnet-0429d69d55dfad9d2"
+
+}
+
+variable "sre_security_group_grafana_id" {
+
+    default = "sg-0b68c85c8877c69e1"
+
+}
