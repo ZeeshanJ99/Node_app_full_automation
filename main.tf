3d60673b86559745c110a567d858ede9ee1b8ebc
WMoorby <WMoorby@spartaglobal.com>
2021-09-24 11:27:49 +0100

    Added NetworkIn, NetworkOut graph

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 ###########################################################################
-resource "aws_cloudwatch_dashboard" "main" {
+resource "aws_cloudwatch_dashboard" "main_dashboard" {
     dashboard_name = "sre_Week-9_project"
 
     dashboard_body = jsonencode(
@@ -22,18 +22,51 @@ resource "aws_cloudwatch_dashboard" "main" {
                     [
                         "AWS/EC2",
                         "CPUUtilization",
-                        "InstanceId",
-                        "i-0aed809624716df82"
+                        "AutoScalingGroupName",
+                        "sre-viktor-tf-asg"
                     ]
                     ],
                     "period":300,
                     "stat":"Average",
                     "region":"eu-west-1",
-                    "title":"App Instance CPU",
+                    "title":"App's Average CPU",
                     "liveData": false,
                     "legend": {
                         "position": "right"
                     }
+                
+                }
+            },
+            {
+                "type":"metric",
+                "x":0,
+                "y":1,
+                "width":12,
+                "height":6,
+                "properties":{
+                    "metrics":[
+                    [
+                        "AWS/EC2",
+                        "NetworkIn",
+                        "AutoScalingGroupName",
+                        "sre-viktor-tf-asg"
+                    ],
+                    [
+                        "AWS/EC2",
+                        "NetworkOut",
+                        "AutoScalingGroupName",
+                        "sre-viktor-tf-asg"
+                    ]
+                    ],
+                    "period":300,
+                    "stat":"Average",
+                    "region":"eu-west-1",
+                    "title":"App's Average CPU",
+                    "liveData": false,
+                    "legend": {
+                        "position": "right"
+                    }
+                
                 }
             }
         ]
